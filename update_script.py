0e8114da602db3c8210183f09885b40bebeffa58
adamj <a.j.jones@sky.com>
2023-09-28 23:59:05 +0100

    fixed reference to jinja/bootstrap html files in send_email.py

--- update_script.py
+++ update_script.py
@@ -7,12 +7,16 @@ from app import count_new_properties
 from email_data.send_email import prepare_email_html, send_email
 from rightmove.geolocation import update_locations
 from rightmove.models import ReviewDates, sqlite_url
-from rightmove.run import download_properties, download_property_data, mark_properties_reviewed
+from rightmove.run import (
+    download_properties,
+    download_property_data,
+    mark_properties_reviewed,
+)
 
 logger = logging.getLogger(__name__)
 sh = logging.StreamHandler()
 sh.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 sh.setFormatter(formatter)
 logger.addHandler(sh)
 
@@ -40,7 +44,9 @@ def main():
     # Send email:
     print("Creating email...")
     engine = create_engine(sqlite_url, echo=False)
-    query = select([ReviewDates.email_id]).order_by(ReviewDates.email_id.desc()).limit(1)
+    query = (
+        select([ReviewDates.email_id]).order_by(ReviewDates.email_id.desc()).limit(1)
+    )
     with Session(engine) as session:
         email_id = session.exec(query).first()
 
