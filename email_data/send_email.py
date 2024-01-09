8860fbe9c1b5f54ea335e6d0dbfd3996672e3b81
adamj <a.j.jones@sky.com>
2024-01-09 17:49:21 +0000

    Updated send_email to use app password, rather than gmail OAuth

--- email_data/send_email.py
+++ email_data/send_email.py
@@ -1,19 +1,14 @@
-import base64
 import json
 import logging
 import os
-import pickle
 import shutil
+import smtplib
 import subprocess
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pathlib import Path
+from typing import List
 
-import httplib2
-from google.oauth2.credentials import Credentials
-from google_auth_httplib2 import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
 from jinja2 import Environment, FileSystemLoader
 from requests import HTTPError
 
@@ -24,55 +19,23 @@ from rightmove.run import get_properties
 logger = logging.getLogger(__name__)
 logger = logging_setup(logger)
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-
-def get_service():
-    logger.info("Getting gmail credentials")
-
-    creds = None
-    token_path = os.path.join(DATA, "token.pickle")
-    if os.path.exists(token_path):
-        with open(token_path, "rb") as token:
-            creds: Credentials = pickle.load(token)
-
-    try:
-        logger.info("Attempting to refresh credentials")
-        http = httplib2.Http()
-        creds.refresh(Request(http))
-    except Exception as e:
-        logger.warning(f"Login required {e}")
-        flow = InstalledAppFlow.from_client_secrets_file(
-            "email_data/credentials.json", SCOPES
-        )
-        creds = flow.run_local_server(port=0)
-
-    with open(token_path, "wb") as token:
-        pickle.dump(creds, token)
-
-    return build("gmail", "v1", credentials=creds)
-
-
-def create_email():
+def create_email(from_email: str, recipients: List[str]) -> MIMEMultipart:
     logger.info("Creating email")
 
-    with open(os.path.join(DATA, "email_details.json"), "r") as f:
-        data = json.load(f)
-        logger.info(f"Recipients: {data['recipients']}")
-
     msg = MIMEMultipart("alternative")
-    msg["From"] = data["from"]
-    msg["To"] = ",".join(data["recipients"])
+    msg["From"] = from_email
+    msg["To"] = ",".join(recipients)
     msg["Subject"] = "Property update"
 
     # HTML Email Content
-    with open("email_data/bootstrap.html", "r") as f:
+    with open(os.path.join(BASE_DIR, "email_data", "bootstrap.html"), "r") as f:
         html_content = f.read()
 
     # Attach HTML Content
     msg.attach(MIMEText(html_content, "html"))
 
-    return {"raw": base64.urlsafe_b64encode(msg.as_bytes()).decode()}
+    return msg
 
 
 def prepare_email_html(review_id) -> bool:
@@ -106,14 +69,22 @@ def prepare_email_html(review_id) -> bool:
 
 
 def send_email():
-    service = get_service()
-    create_message = create_email()
+    with open(os.path.join(DATA, "secrets.json"), "r") as f:
+        secrets = json.load(f)
+        email = secrets["email"]
+        password = secrets["password"]
+
+    with open(os.path.join(DATA, "email_details.json"), "r") as f:
+        data = json.load(f)
+        recipients = data["recipients"]
+
+    message = create_email(email, recipients)
 
     try:
-        message = (
-            service.users().messages().send(userId="me", body=create_message).execute()
-        )
-        logger.info(f'sent message to {message} Message Id: {message["id"]}')
+        smtp_server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        smtp_server.login(email, password)
+        smtp_server.sendmail(email, recipients, message.as_string())
+        logger.info(f'Sent message to email(s): {", ".join(recipients)}')
     except HTTPError as error:
         logger.info(f"An error occurred: {error}")
 
